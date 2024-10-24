e10ba286934b2972be967be23e1f388407c90242
Ashley Wright <ashley@msrandom.net>
2024-10-24 05:38:44 +0300

    Add logic for chain interactions

---
+++ base/build.rs
@@ -0,0 +1,33 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
+use parity_scale_codec::Decode;
+use subxt_codegen;
+use subxt_codegen::CodegenBuilder;
+use subxt_codegen::fetch_metadata::{fetch_metadata_from_url, MetadataVersion};
+use subxt_metadata::Metadata;
+
+#[tokio::main]
+async fn main() {
+    let endpoint = env::var_os("CHAIN_ENDPOINT")
+        .map(|s| s.into_string().unwrap())
+        .unwrap_or("wss://entrypoint-finney.opentensor.ai:443".into());
+
+    let endpoint: &str = &endpoint;
+
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let metadata_path = Path::new(&out_dir).join("metadata.rs");
+
+    let metadata_bytes = fetch_metadata_from_url(endpoint.try_into().unwrap(), MetadataVersion::Latest).await.unwrap();
+    let mut metadata_bytes: &[u8] = &metadata_bytes;
+    let metadata = Metadata::decode(&mut metadata_bytes).unwrap();
+
+    let codegen = CodegenBuilder::new();
+
+    let code = codegen.generate(metadata).unwrap();
+    let mut file_output = File::create(metadata_path).unwrap();
+
+    file_output.write_fmt(format_args!("{code}")).unwrap();
+}
---
+++ base/src/lib.rs
@@ -0,0 +1,94 @@
+use parity_scale_codec::{Compact, Decode};
+use subxt::client::OnlineClient;
+use subxt::{Config, SubstrateConfig};
+use anyhow::Result;
+
+include!(concat!(env!("OUT_DIR"), "/metadata.rs"));
+
+type SubtensorConfig = SubstrateConfig;
+type AccountId = <SubtensorConfig as Config>::AccountId;
+
+#[derive(Decode, Default, Clone, Debug)]
+pub struct AxonInfo {
+    pub block: u64,
+    pub version: u32,
+    pub ip: u128,
+    pub port: u16,
+    pub ip_type: u8,
+    pub protocol: u8,
+    _placeholder1: u8,
+    _placeholder2: u8,
+}
+
+#[derive(Decode, Default, Clone, Debug)]
+pub struct PrometheusInfo {
+    pub block: u64,
+    pub version: u32,
+    pub ip: u128,
+    pub port: u16,
+    pub ip_type: u8,
+}
+
+#[derive(Decode, Clone, Debug)]
+pub struct NeuronInfoLite {
+    pub hotkey: AccountId,
+    pub coldkey: AccountId,
+    pub uid: Compact<u16>,
+    pub netuid: Compact<u16>,
+    pub active: bool,
+    pub axon_info: AxonInfo,
+    pub prometheus_info: PrometheusInfo,
+    pub stake: Vec<(AccountId, Compact<u64>)>,
+    pub rank: Compact<u16>,
+    pub emission: Compact<u64>,
+    pub incentive: Compact<u16>,
+    pub consensus: Compact<u16>,
+    pub trust: Compact<u16>,
+    pub validator_trust: Compact<u16>,
+    pub dividends: Compact<u16>,
+    pub last_update: Compact<u64>,
+    pub validator_permit: bool,
+    pub pruning_score: Compact<u16>,
+}
+
+pub struct Subtensor {
+    client: OnlineClient<SubtensorConfig>,
+}
+
+impl Subtensor {
+    pub async fn new(url: impl AsRef<str>) -> Result<Self> {
+        Ok(Self {
+            client: OnlineClient::from_url(url).await?
+        })
+    }
+
+    pub async fn get_neurons(&self, netuid: u16) -> Result<Vec<NeuronInfoLite>> {
+        let neurons_payload = api::apis()
+            .neuron_info_runtime_api()
+            .get_neurons_lite(netuid);
+
+        let response = self.client
+            .runtime_api()
+            .at_latest()
+            .await?
+            .call(neurons_payload)
+            .await?;
+
+        Ok(Vec::<NeuronInfoLite>::decode(&mut response.as_ref())?)
+    }
+
+    pub fn set_weights(&self, netuid: u16, weights: Vec<(u16, u16)>, version_key: u64) -> Result<()> {
+        let set_weights_payload = api::tx()
+            .subtensor_module()
+            .set_weights(
+                netuid,
+                weights.iter().map(|(uid, _)| *uid).collect(),
+                weights.iter().map(|(_, weight)| *weight).collect(),
+                version_key,
+            );
+
+        let response = self.client.tx();
+
+        Ok(())
+    }
+}
